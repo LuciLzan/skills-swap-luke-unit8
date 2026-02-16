e528dcb3a3e559e78858b8463c71a467545bd0ca
Luke Laurenzana <laurenzana.luke@ic.edu>
2026-02-15 19:17:11 -0600

    implemented calculateTotalCost, added tests

--- skillswap-functions.js
+++ skillswap-functions.js
@@ -2,5 +2,9 @@ function filterSkillsByCategory(skills,category) {
     return category.toLowerCase() === "all" ? skills : skills.filter(skill => skill.category.toLowerCase() === category.toLowerCase())
 }
 
+function calculateTotalCost(rate,hours) {
+    return rate*hours
+}
 
-module.exports = { filterSkillsByCategory }
+
+module.exports = { filterSkillsByCategory, calculateTotalCost}
